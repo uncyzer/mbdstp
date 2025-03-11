76aa8aa2276acaf181060b0a738bca86015222e4
Enzo ROCAMORA <enzo.rocamora@etu.unice.fr>
2025-03-11 11:10:28 +0100

    feat(customer): yipee

--- customer/src/main/java/fr/rocamora/barrali/customer/CustomerApplication.java
+++ customer/src/main/java/fr/rocamora/barrali/customer/CustomerApplication.java
@@ -1,7 +1,13 @@
 package fr.rocamora.barrali.customer;
 
+import fr.rocamora.barrali.customer.Entities.Customer;
+import fr.rocamora.barrali.customer.Repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CustomerApplication {
@@ -10,4 +16,22 @@ public class CustomerApplication {
         SpringApplication.run(CustomerApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository) {
+        return args -> {
+            List<Customer> customerList = List.of(
+                    Customer.builder()
+                            .firstName("John")
+                            .lastName("Doe")
+                            .email("john.doe@etu.unice.fr")
+                            .build(),
+                    Customer.builder()
+                            .firstName("Jane")
+                            .lastName("Doe")
+                            .email("jane.doe@etu.unice.fr")
+                            .build()
+            );
+            customerRepository.saveAll(customerList);
+        };
+    }
 }
