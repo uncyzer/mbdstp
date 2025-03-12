63a1fcb8318388a88a01214e4931b66fc9c3b00a
Enzo ROCAMORA <enzo.rocamora@etu.unice.fr>
2025-03-12 10:06:02 +0100

    Discovery service setup

--- account/src/main/java/fr/rocamora/barrali/account/Controllers/AccountController.java
+++ account/src/main/java/fr/rocamora/barrali/account/Controllers/AccountController.java
@@ -6,12 +6,14 @@ import fr.rocamora.barrali.account.Repositories.AccountRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
+@RequestMapping
 public class AccountController {
     private final AccountRepository accountRepository;
     private final CustomerClient customerClient;
---
+++ discovery/src/test/java/fr/rocamora/barrali/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rocamora.barrali.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
