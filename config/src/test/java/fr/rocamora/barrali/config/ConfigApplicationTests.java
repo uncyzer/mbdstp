6e93047bb1109f822a5c8597b0c7d55d855fa752
Enzo ROCAMORA <enzo.rocamora@etu.unice.fr>
2025-03-12 11:04:38 +0100

    Config server

---
+++ config/src/test/java/fr/rocamora/barrali/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rocamora.barrali.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
