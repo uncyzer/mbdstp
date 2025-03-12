5d2ca745361783750324b1ec4de25638ad1c9b44
Enzo ROCAMORA <enzo.rocamora@etu.unice.fr>
2025-03-12 10:28:05 +0100

    coucou le circuit breaker !

--- account/src/main/java/fr/rocamora/barrali/account/Client/CustomerClient.java
+++ account/src/main/java/fr/rocamora/barrali/account/Client/CustomerClient.java
@@ -1,16 +1,33 @@
 package fr.rocamora.barrali.account.Client;
 
 import fr.rocamora.barrali.account.Entities.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "customers", url = "http://localhost:8080/")
+@FeignClient(name = "CUSTOMER")
 public interface CustomerClient {
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getDefaultCustomer")
     public Customer getCustomer(@PathVariable Long id);
     @GetMapping("/customers")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getAllCustomers")
     public List<Customer> getCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception exception)
+    {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName("Not available");
+        customer.setLastName("Not available");
+        customer.setEmail("Not available");
+        return customer;
+    }
+    default List<Customer> getAllCustomers(Exception exception)
+    {
+        return List.of();
+    }
 }
