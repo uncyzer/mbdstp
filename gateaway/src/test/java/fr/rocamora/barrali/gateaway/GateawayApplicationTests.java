1ba922004ad1b4dc18aaa246873cbf619b692b12
Enzo ROCAMORA <enzo.rocamora@etu.unice.fr>
2025-03-12 09:57:13 +0100

    Gateway ggwp

---
+++ gateaway/src/test/java/fr/rocamora/barrali/gateaway/GateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rocamora.barrali.gateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
